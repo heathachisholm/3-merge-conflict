21e2ad5cef2be99726da2c0556c23f02039125ed
Kornel Lesiński <kornel@geekhood.net>
2013-07-31 15:24:57 +0100

    Initial

---
+++ kitchen.cpp
@@ -0,0 +1,35 @@
+
+class Kitchen
+{
+    lightbulb = Lighbulb(BURNT_OUT);
+};
+
+
+class Cupboard
+{
+    inventory[] = {Lighbulb(NEW)};
+
+    shelf.screws[0] = UNSCREWED;
+};
+
+
+class Drawer
+{
+    inventory[] = {Screwdriver()};
+
+    void open() {
+        make_squeaky_noise();
+    }
+};
+
+
+class WD40 {
+    availalbe = 0;
+};
+
+
+class Car {
+    void start() {
+        make_funny_noises();
+    }
+};
